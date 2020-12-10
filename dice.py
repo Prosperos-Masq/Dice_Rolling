f8bdb5ea7b77009fbf866cf8ae85053ed8c565cd
Prospero <60527636+Prosperos-Masq@users.noreply.github.com>
2020-12-10 02:43:01 -0600

    update 1
    learned about defining functions

--- dice.py
+++ dice.py
@@ -1,19 +1,23 @@
 import random
 
-l = range(1,7)
-dice = random.choice(l)
+def roll_dice():
+    dice = random.choice(chance)
+    return dice
+
+chance = range(1,7)
+roll_dice()
 ask = input("Would you like to roll the dice (y/n)?: ").strip().lower()
 
 if ask == "y":
-    print("Dice has rolled a {}!".format(dice))
+    print("Dice has rolled a {}!".format(roll_dice()))
 else:
     print("Okay, maybe another time?")
     
 while True:
-    dice = random.choice(l)
+    roll_dice()
     again = input("Would you like to roll again (y/n)?: ").strip().lower()
     if again == "y":
-        print("Dice has rolled a {}!".format(dice))
+            print("Dice has rolled a {}!".format(roll_dice()))
     else:
         print("Another time then!")
         break
